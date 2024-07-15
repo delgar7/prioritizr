420f9f6551bb9a27c4dde51296162ca3d504f8bc
Delano Garcia <delanogarciaillera@gmail.com>
2024-07-15 17:18:14 +0200

    Installed AutoAnimate

---
+++ src/hooks/useAutoAnimate.js
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
+
+
+function useAutoAnimate() {
+  return (
+    <div>useAutoAnimate</div>
+  )
+}
+
+export default useAutoAnimate
