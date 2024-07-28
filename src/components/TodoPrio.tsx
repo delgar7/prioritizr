01fb6587998a7f249522496b4aff28577ed0f4a5
Delano Garcia <delanogarciaillera@gmail.com>
2024-07-28 16:43:06 +0200

    random changes

--- src/components/TodoPrio.tsx
+++ src/components/TodoPrio.tsx
@@ -5,19 +5,17 @@ interface ITodoPrios {
 
 function TodoPrio({ value, onChange }: ITodoPrios) {
   return (
-    <li className="relative">
-      <select
-        className="w-40 h-8 mr-4 font-bold text-gray-600 border-2 rounded hover:border-gray-400 focus:outline-none"
-        value={value}
-        onChange={onChange}
-      >
-        <option value="urgent">🔥 Urgent</option>
-        <option value="high">🟥 High</option>
-        <option value="medium">🟧 Medium</option>
-        <option value="low">🟨 Low</option>
-        <option value="none">⬜️ None</option>
-      </select>
-    </li>
+    <select
+      className="w-40 h-8 mr-4 font-bold text-gray-600 border-2 rounded hover:border-gray-400 focus:outline-none"
+      value={value}
+      onChange={onChange}
+    >
+      <option value="urgent">🔥 Urgent</option>
+      <option value="high">🟥 High</option>
+      <option value="medium">🟧 Medium</option>
+      <option value="low">🟨 Low</option>
+      <option value="none">⬜️ None</option>
+    </select>
   );
 }
 
