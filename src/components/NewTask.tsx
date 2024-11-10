76a29c8180da08f8e3a776f1f330896d2be76f84
Delano Garcia <115837629+delgar7@users.noreply.github.com>
2024-11-10 12:40:02 +0100

    Add dark mode border color class

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -10,11 +10,16 @@ interface NewTaskProps {
 const NewTask: React.FC<NewTaskProps> = ({ todos, setTodos }) => {
     const handleAdd = () => handleAddTodo(todos, setTodos, "Todo");
     return (
-        <div className="flex items-center px-10 py-5 mb-6 space-x-8 border-2 border-dashed rounded-md bg-primary">
-            <button className="text-4xl font-inter" onClick={handleAdd}>
+        <div className="flex items-center px-10 py-5 mb-6 space-x-8 border-2 border-dashed rounded-md bg-primary dark:bg-darkBackground dark:border-[#88919F]">
+            <button
+                className="text-4xl font-inter dark:text-darkText"
+                onClick={handleAdd}
+            >
                 +
             </button>
-            <h2 className="pt-1 text-2xl font-light font-inter">New Task</h2>
+            <h2 className="pt-1 text-2xl font-light font-inter dark:text-darkText">
+                New Task
+            </h2>
         </div>
     );
 };
